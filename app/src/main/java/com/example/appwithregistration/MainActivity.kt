19dd5c16b1ec04c32edfa407000fc5eba796967f
demorgan <drankov.alex@yandex.ru>
2024-05-01 02:12:21 +0500

    registration

--- app/src/main/java/com/example/appwithregistration/MainActivity.kt
+++ app/src/main/java/com/example/appwithregistration/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.appwithregistration
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +14,28 @@ class MainActivity : AppCompatActivity() {
         val userLogin: EditText = findViewById(R.id.user_login)
         val userEmail: EditText = findViewById(R.id.user_email)
         val userPassword: EditText = findViewById(R.id.user_passwd)
+        val registrationButton: Button = findViewById(R.id.button_reg)
 
+
+        registrationButton.setOnClickListener {
+            val login = userLogin.text.toString().trim()
+            val email = userEmail.text.toString().trim()
+            val password = userPassword.text.toString().trim()
+
+            if (login.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Заполните все поля", Toast.LENGTH_LONG).show()
+            }
+            else {
+                val user = User(login, email, password)
+
+                val db = DBhelper(this, null)
+                db.addUser(user)
+                Toast.makeText(this, "Пользователь $login успешно добавлен", Toast.LENGTH_LONG).show()
+
+                userLogin.text.clear()
+                userEmail.text.clear()
+                userPassword.text.clear()
+        }
     }
     }
 }
---
+++ app/src/main/java/com/example/appwithregistration/User.kt
@@ -0,0 +1,4 @@
+package com.example.appwithregistration
+
+class User(val login: String, val email: String, val password: String) {
+}
