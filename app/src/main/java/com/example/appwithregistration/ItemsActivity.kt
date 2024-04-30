e145fb7d7778c6230d0a1506951ff42d75d438bb
demorgan <drankov.alex@yandex.ru>
2024-05-01 03:49:41 +0500

    market

--- app/src/main/java/com/example/appwithregistration/ItemsActivity.kt
+++ app/src/main/java/com/example/appwithregistration/ItemsActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class ItemsActivity : AppCompatActivity() {
@@ -15,8 +16,11 @@ class ItemsActivity : AppCompatActivity() {
         val itemsList: RecyclerView = findViewById(R.id.itemsList)
         val items = arrayListOf<Item>()
 
-        items.add(Item(1, "1", "Tree", "Tree description", "Tree text", 100))
-        items.add(Item(2, "2", "Plastic Tree", "Tree description", "Tree text", 200))
-        items.add(Item(3, "3", "Real Tree", "Tree description", "Tree text", 300))
+        items.add(Item(1, "first", "Tree", "Tree description", "Tree text", 100))
+        items.add(Item(2, "second", "Plastic Tree", "Tree description", "Tree text", 200))
+        items.add(Item(3, "third", "Real Tree", "Tree description", "Tree text", 300))
+
+        itemsList.layoutManager = LinearLayoutManager(this)
+        itemsList.adapter = ItemsAdapter(items, this)
       }
 }
